6f91da15d674e60eda013f2d4240392cb900b700
张玲玉 <zhanglingyu@58ganji.com>
2017-05-04 22:01:19 +0800

    提交

---
+++ FFCategory/FFCategory/Category/NSObject+FFSafe.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+FFSafe.h
+//  FFCategory
+//
+//  Created by 张玲玉 on 2017/5/4.
+//  Copyright © 2017年 张玲玉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (FFSafe)
+
+@end
---
+++ FFCategory/FFCategory/Category/NSObject+FFSafe.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+FFSafe.m
+//  FFCategory
+//
+//  Created by 张玲玉 on 2017/5/4.
+//  Copyright © 2017年 张玲玉. All rights reserved.
+//
+
+#import "NSObject+FFSafe.h"
+
+@implementation NSObject (FFSafe)
+
+@end
